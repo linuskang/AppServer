fae5d3e3c7c85ff564e0ed049471546b70dbaf8e
linuskang <linus@kang.id.au>
2025-05-10 10:11:13 +1000

    auth routes.

--- App Services/LinusOnlineServices/main.js
+++ App Services/LinusOnlineServices/main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 const { logger } = require('../../src/utils/winston');
+const { auth } = require('../../src/routes/middleware/sysauth');
 const log = logger();
 
 log.info("Linus Online Services loaded.");
@@ -9,5 +10,11 @@ log.info("Linus Online Services loaded.");
 router.get('/', (req, res) => {
     res.json({ service_name: 'Linus Online Services', status: '200 OK', version: '1.0.0' });
 });
+
+router.get('/authroute', auth, (req, res) => {
+    res.json({ pass: 'true' });
+});
+
+// curl -X GET https://app.linuskang.au/lkang/authroute -H "Authorization: Bearer linuskang09032011.au"
 
 module.exports = router;
